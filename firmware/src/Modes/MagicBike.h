7abb9cf4d51d4fbb824f88b0a20709101fdfa290
ibi sum <seclorum@icloud.com>
2022-10-16 18:10:18 +0200

    bike ui: switch back/front roles

--- firmware/src/Modes/MagicBike.h
+++ firmware/src/Modes/MagicBike.h
@@ -21,7 +21,43 @@ private:
 
 	virtual void start() {
 
-		if (_bike.role == MS3KG_App_Bike_Role_FRONT_LIGHT) {
+
+	}
+
+	virtual void stop(void) {
+	}
+
+	virtual bool step(void) {
+
+		int new_role = _bike.role;
+		int blink_mode = _bike.
+	
+		msSystem.msLEDs.loadBuffer(msGlobals.ggRGBLEDBuf);
+		msSystem.msLEDs.updateLEDs();
+		delay(10);
+
+		if (msSystem.msButtons.msBtnALongHit) {
+			new_role--;
+		}
+
+		if (msSystem.msButtons.msBtnAHit) {
+		}
+
+ 		if (msSystem.msButtons.msBtnBLongHit) {
+			new_role++;
+		}
+                                        0
+		if (msSystem.msButtons.msBtnBHit) {
+		}
+
+		if(new_role<MS3KG_App_Bike_Role_FRONT_LIGHT)
+			new_role=MS3KG_App_Bike_Role_BACK_LIGHT;
+		if(new_role>MS3KG_App_Bike_Role_BACK_LIGHT)
+			new_role=MS3KG_App_Bike_Role_FRONT_LIGHT;
+
+		_bike.role = (MS3KG_App_Bike_Role)new_role;
+
+ 		if (_bike.role == MS3KG_App_Bike_Role_FRONT_LIGHT) {
 
 			for(int i=0;i<RGB_BUFFER_SIZE;i+=4) {
    	 			msGlobals.ggRGBLEDBuf[i] = msGlobals.ggBrightness | 0xe0;
@@ -31,15 +67,16 @@ private:
 			}
 		}
 
-	}
+  		if (_bike.role == MS3KG_App_Bike_Role_BACK_LIGHT) {
 
-	virtual void stop(void) {
-	}
-
-	virtual bool step(void) {
-		msSystem.msLEDs.loadBuffer(msGlobals.ggRGBLEDBuf);
-		msSystem.msLEDs.updateLEDs();
-		delay(10);
+			for(int i=0;i<RGB_BUFFER_SIZE;i+=4) {
+   	 			msGlobals.ggRGBLEDBuf[i] = msGlobals.ggBrightness | 0xe0;
+   	 			msGlobals.ggRGBLEDBuf[i+1] = i < 32 ? 255 : 0;
+   	 			msGlobals.ggRGBLEDBuf[i+2] = i < 32 ? 255 : 255;
+	   	 		msGlobals.ggRGBLEDBuf[i+3] = i < 32 ? 255 : 0;
+			}
+		}
+ 
 		return true;
 	}
 };
