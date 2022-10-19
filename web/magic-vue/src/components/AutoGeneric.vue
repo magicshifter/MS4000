418b0c22171df3747a1492d25f9cc1f3c76ab58b
Philipp Tiefenbacher <wizards23@gmail.com>
2022-10-19 23:03:00 +0200

    drafting AutoGEneric control for AutoInterface, copied lines from web/app/AutoInnterface/AutoControl; react component

---
+++ web/magic-vue/src/components/AutoGeneric.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+
+const { field , value, onChange } = this.props
+
+const root = field.root
+const lookup = root.lookup(field.type)
+const isEnum = lookup instanceof protobuf.Enum
+const isType = lookup instanceof protobuf.Type
+
+
+</script>
