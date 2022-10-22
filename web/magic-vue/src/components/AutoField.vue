1f450daca978f45de389c647b44345593640116b
Philipp Tiefenbacher <wizards23@gmail.com>
2022-10-22 13:32:16 +0200

    FINALLY: IT WORKS!, recursively showing types and their fields (which have a type)

---
+++ web/magic-vue/src/components/AutoField.vue
@@ -0,0 +1,47 @@
+
+<script setup lang="ts">
+import {reactive, watchEffect, ref} from "vue"
+import AutoControll from "../components/AutoControll.vue"
+import AutoType from "./AutoType.vue"
+
+const props = defineProps<{
+    field: any,
+    value: any,
+    onChange: any,
+}>()
+
+// const state = reactive({ 
+//   modes: []
+// })
+
+const {field, value} = props 
+const {type, root, name } = field
+
+
+let t = undefined
+try {
+    t = root.lookupType(type)
+}
+catch (ex) {
+    console.warn("could not lookup Type " + type + " in:", field)
+}
+// const rootType = props.field
+
+
+// const fields = rootType?.fields
+// // const modes = Object.keys(rootType.fields).map((name) => ({
+// //     id: name,
+// //     t: rootType.fields[name],
+// //   }))
+
+</script>
+
+<template>
+  <div>
+    <h2>Auto Field</h2>
+    <div>field: {{field}}</div>
+    <div>type: {{type}}</div>
+    <!-- rio {{root}}ot: -->
+    <AutoType :type="t" v-if="t != undefined"/>
+  </div>
+</template>
--- web/magic-vue/src/components/AutoType.vue
+++ web/magic-vue/src/components/AutoType.vue
@@ -1,6 +1,8 @@
+
 <script setup lang="ts">
 import {reactive, watchEffect, ref} from "vue"
 import AutoControll from "../components/AutoControll.vue"
+import AutoField from "./AutoField.vue"
 
 const props = defineProps<{
     type: any,
@@ -18,10 +20,7 @@ const root = type.root
 
 
 const fields = type?.fields
-// const modes = Object.keys(rootType.fields).map((name) => ({
-//     id: name,
-//     t: rootType.fields[name],
-//   }))
+const fieldIndex = fields ? Object.keys(fields).map((name) => fields[name]) : []
 
 </script>
 
@@ -30,6 +29,12 @@ const fields = type?.fields
     <h1>Auto Type</h1>
     <div>Type: {{type}}</div>
     <div>Fields: {{fields ?  fields : "none"}}</div>
+    <div>
+        <div v-for="f in fieldIndex">
+            field: {{f}}
+            <AutoField :field="f" />
+        </div>
+    </div>
     <!-- <div v-for="mode in modes">
       <AutoMode :mode="mode" :root="root"/>
     </div> -->
