7afbb7b53761ba6baf7cd8a2ce47018503844672
Philipp Tiefenbacher <wizards23@gmail.com>
2022-10-24 09:25:12 +0200

    actually decoding a ProtoBuff base64 encoded state frfom a shifter :) current mode seems to work :)

--- web/magic-vue/src/views/AutoInterfaceView.vue
+++ web/magic-vue/src/views/AutoInterfaceView.vue
@@ -41,6 +41,19 @@ promise.then((root) => {
 
   const rootType = root["MS4"]
 
+
+
+  // const testDataBase64 = "ChIIADoOCAEQARoICP8BEHkY/wE="
+  const testDataBase64 = "ChIIBDoOCAEQARoICP8BEHkY/wE=" // current = 4
+  // const testDataBase64 = "ChIIAToOCAEQARoICP8BEHkY/wE=" // current = 1
+
+  const decodedObj = decodeBase64(testDataBase64, rootType)
+  console.log("decoded MS4", decodedObj)
+
+  
+
+
+
   state.rootType = rootType
   state.root = root
   state.modes = Object.keys(rootType.fields).map((name) => ({
@@ -49,6 +62,77 @@ promise.then((root) => {
   }))
   
 })
+
+
+function stringToArray(bufferString) {
+  var array = new Uint8Array(new ArrayBuffer(bufferString.length));
+
+  for (var i = 0; i < bufferString.length; i++) {
+    array[i] = bufferString.charCodeAt(i);
+  }
+  return array
+}
+
+function decodeBase64(text, pbType) {
+  var decoded = atob(text)
+    console.log("b64 decoded", decoded)
+    var u8a = stringToArray(decoded);
+    //dumpU8(u8a)
+    console.log("u8", u8a)
+
+    try {
+      const shifterState = pbType.decode(u8a);
+      console.log("shifterState decoded MS4", shifterState)
+
+      var object = pbType.toObject(shifterState, {
+        longs: undefined,
+        enums: undefined,
+        bytes: undefined,
+      });
+
+      console.log("after toObject decoded MS4", object)
+
+      return object
+    }
+    catch (ex) {
+      console.error("Decoding failed", ex)
+    }
+}
+
+
+  // // const host = 'http://MS3000-7161.local'
+  // const host = 'http://192.168.4.1'
+  // // const host = ""
+
+  // fetch(host + '/protobuf')
+  //   .then(data => data.text())
+  //   .then(text => {
+  //     var decoded = atob(text)
+  //     console.log("b64 decoded", decoded)
+  //     var u8a = stringToArray(decoded);
+  //     //dumpU8(u8a)
+  //     console.log("u8", u8a)
+
+  //     try {
+  //       const shifterState = pb.MS3KG.decode(u8a);
+
+  //       var object = pb.MS3KG.toObject(shifterState, {
+  //         longs: undefined,
+  //         enums: undefined,
+  //         bytes: undefined,
+  //       });
+
+  //       dispatch(configDownloadSuccess())
+  //       dispatch(configUpdate(object))
+  //     }
+  //     catch (ex) {
+  //       dispatch(configDownloadFail("shifterstate fetch decode error: " + ex))
+  //     }
+  //   })
+  //   .catch(error => {
+  //     console.error('Error:', error)
+  //     dispatch(configDownloadFail(error))
+  //   });
 
 </script>
 
